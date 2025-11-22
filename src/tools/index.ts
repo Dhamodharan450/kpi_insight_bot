d9653cf89390650846c08ab63e3402244eebcf2a
Dhamodharan450 <damodharan@hyperready.tech>
2025-11-22 11:36:38 +0530

    Update index.ts

--- src/tools/index.ts
+++ src/tools/index.ts
@@ -1,5 +1,6 @@
-export * from './db_kpi_tools';
-export * from './db_insight_tools';
+export {  listTablesToolTool, listColumnsTool, saveKPITool } from './db_kpi_tools';
+// also possible : export {  listTablesToolTool, listColumnsTool,runQueryTool, saveKPITool } from './db_kpi_tools';
+export { fetchKPIsTool, runQueryTool,saveInsightTool } from './db_insight_tools';
 export { ensureTables, pool } from './db_shared';
 
 export { generateSQLTool } from './sql_generator';
